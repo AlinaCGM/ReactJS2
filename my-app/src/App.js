89eb12cd2515dc13a635e3165b13da3e2b1ca879
AlinaCGM <alinbogz@gmail.com>
2021-06-02 20:59:57 +0200

    lesson7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import Employee from "./Employee"
 
-function App() {
+const employee = [
+  {name: "Alina", 
+    id: "ABC"
+  },
+  {name: "dINU", 
+    id: "DEF"
+  }
+];
+
+export default function App() {
+
+  const firstEmployee = employee[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+    
+         <Employee {...firstEmployee}/>
     </div>
   );
+
 }
 
-export default App;
